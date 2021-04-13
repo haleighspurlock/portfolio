5344e630b43b54848b68aa8c7f489064d74239fe
Haleigh Spurlock <haleighspurlock@hotmail.com>
2021-04-13 19:20:26 -0400

    added header with working nav links, footer not styled but on all pages as well. main container switches with each page link

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './components/Header';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
 import './App.css';
+import './index.css';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+
+        <Container>
+            <Route exact path='/' component={Home}/>
+            <Route exact path='/home' component={Home}/>
+            <Route exact path='/projects' component={Projects}/>
+            <Route exact path='/contact' component={Contact}/>
+        </Container>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
