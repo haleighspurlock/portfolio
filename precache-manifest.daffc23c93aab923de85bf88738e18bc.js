554aa300986b592bf4af2284c7eb41826a285ae3
Haleigh Twohy <haleightwohy@Haleighs-MBP.lan>
2023-11-02 10:16:31 -0400

    Updates

---
+++ precache-manifest.daffc23c93aab923de85bf88738e18bc.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10bd7c686700257fec07119a4653553f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68da326959b5f031ed6c",
+    "url": "/static/css/3.6a352120.chunk.css"
+  },
+  {
+    "revision": "233ad99d05d76dff1325",
+    "url": "/static/css/4.6e14a8dd.chunk.css"
+  },
+  {
+    "revision": "10f6a6598ac06a8ebc35",
+    "url": "/static/css/main.0738e884.chunk.css"
+  },
+  {
+    "revision": "ffbef262a2b0c40475ca",
+    "url": "/static/js/2.3538d0fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3538d0fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68da326959b5f031ed6c",
+    "url": "/static/js/3.a7c5b718.chunk.js"
+  },
+  {
+    "revision": "233ad99d05d76dff1325",
+    "url": "/static/js/4.d9cc4b78.chunk.js"
+  },
+  {
+    "revision": "10f6a6598ac06a8ebc35",
+    "url": "/static/js/main.bfc8f477.chunk.js"
+  },
+  {
+    "revision": "459fa5a16c4e397c586b",
+    "url": "/static/js/runtime-main.488cff33.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "7aa20e5b73386c0440af9701b1ef8e37",
+    "url": "/static/media/Doggle.7aa20e5b.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "957f2b2f945a1d91e1f86d882d23415d",
+    "url": "/static/media/QuirkyQuiz.957f2b2f.png"
+  },
+  {
+    "revision": "bc47c11755532aa3512e8a58d8bf155b",
+    "url": "/static/media/charterlogo.bc47c117.png"
+  },
+  {
+    "revision": "f7f5204a7d296ae118d9da306e4b47f0",
+    "url": "/static/media/fitness-dashboard.f7f5204a.png"
+  },
+  {
+    "revision": "b43d80cf8d8924c15d21e84bdfaf7db6",
+    "url": "/static/media/homepage.b43d80cf.png"
+  },
+  {
+    "revision": "a249aceb085da9892b52eb844317f8ad",
+    "url": "/static/media/software.a249aceb.gif"
+  },
+  {
+    "revision": "af3fa5b022d3451a8d020243d05b5361",
+    "url": "/static/media/tlg.af3fa5b0.png"
+  }
+]);
